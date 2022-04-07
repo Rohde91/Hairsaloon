d0f278bbb2a887157d165935db0dc8901b1ded6b
Marc3724 <marc402m@edu.easj.dk>
2022-04-07 13:17:18 +0200

    added Login.fxml on action (button) and connected it to TryUserLogin

--- src/main/java/com/groupwork/hairsaloon/HairsaloonController.java
+++ src/main/java/com/groupwork/hairsaloon/HairsaloonController.java
@@ -1,15 +1,24 @@
 package com.groupwork.hairsaloon;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HairsaloonController {
+    public TextField usernameInputfield;
+    public PasswordField passwordInputfield;
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public void loginUserActionButton(ActionEvent actionEvent) {
+        String email = usernameInputfield.getText();
+        String password = passwordInputfield.getText();
+        Trickster.mysql msql = Trickster.mysql.getInstance();
+        msql.TryUserLogin(email,password);
     }
 }
 
