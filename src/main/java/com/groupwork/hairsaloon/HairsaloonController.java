60fe6adc69ef09f750883b9e11b904b528483a1c
alla2297 <alla2297@edu.easj.dk>
2022-04-07 17:13:57 +0200

    move "Menu.fxml" and made scene switch from "login.fxml" to "Menu.fxml" And look carefully at "Hairsaloon.iml" because i don't know what i has changed in it

--- src/main/java/com/groupwork/hairsaloon/HairsaloonController.java
+++ src/main/java/com/groupwork/hairsaloon/HairsaloonController.java
@@ -2,12 +2,22 @@ package com.groupwork.hairsaloon;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.util.Objects;
 
 public class HairsaloonController {
+    @FXML
+    private Stage stage;
+    @FXML
     public TextField usernameInputfield;
+    @FXML
     public PasswordField passwordInputfield;
 
 
@@ -18,7 +28,18 @@ public class HairsaloonController {
         String password = passwordInputfield.getText();
         Trickster.mysql msql = Trickster.mysql.getInstance();
         msql.TryUserLogin(email,password);
+
+        // Switch scene to Menu Scene
+        if (msql.userType(email).equals("Employee")||msql.userType(email).equals("Customer")){
+            try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Menu.fxml")));
+            stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
-
-//button not there.
