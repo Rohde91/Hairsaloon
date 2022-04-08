501544681d6df5a14cfc4635194c2799ba8b1e90
dgh81 <89967372+dgh81@users.noreply.github.com>
2022-04-08 14:04:13 +0200

    oprettet booking klasse etc.

--- src/main/java/Trickster/Main.java
+++ src/main/java/Trickster/Main.java
@@ -3,6 +3,8 @@ package Trickster;
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
 import java.sql.Time;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class Main {
 
@@ -11,12 +13,17 @@ public class Main {
         mysql msql = mysql.getInstance();
         //Employee user1 = new Employee("Karen Klippesen", "kk@klipmig.dk", "Klippevej 14 Hårstrup 4100", "11223344", "1111");
         //msql.editCustomer("1","newEmail@test.dk","666666","Zealand","admin2.0","newEmail@test.dk");
-
         User user1 = msql.TryUserLogin("e@e.dk","ee");
-
 
         visual f = new visual();
         f.look();
+
+        Time t = new Time(1);
+        LocalDate dt = LocalDate.of(2022,4,8);
+        Booking b = new Booking(1,t,dt,1,1);
+        msql.createBookingInSQL(b);
+
+
 
     }
 }
