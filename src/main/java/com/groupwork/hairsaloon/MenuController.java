ca7a858861b5058754d5d40c9acb0bbe0d417c26
alla2297 <alla2297@edu.easj.dk>
2022-04-08 12:34:37 +0200

    Logout in Menu Scene

--- src/main/java/com/groupwork/hairsaloon/MenuController.java
+++ src/main/java/com/groupwork/hairsaloon/MenuController.java
@@ -2,7 +2,15 @@ package com.groupwork.hairsaloon;
 //com.groupwork.hairsaloon.MenuController
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class MenuController {
 
@@ -93,7 +101,15 @@ public class MenuController {
 
     @FXML
     void terminateSession(ActionEvent event) {
-
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Login.fxml")));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
