74ceb9e88ebcd6e3009c75ec8b6070d362f2c2eb
alla2297 <alla2297@edu.easj.dk>
2022-04-08 10:36:47 +0200

    Scene switch to menu Import

--- src/main/java/com/groupwork/hairsaloon/HairsaloonController.java
+++ src/main/java/com/groupwork/hairsaloon/HairsaloonController.java
@@ -2,13 +2,22 @@ package com.groupwork.hairsaloon;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class HairsaloonController {
     public TextField usernameInputfield;
     public PasswordField passwordInputfield;
+
+
 
 
     @FXML
@@ -16,18 +25,19 @@ public class HairsaloonController {
         String email = usernameInputfield.getText();
         String password = passwordInputfield.getText();
         Trickster.mysql msql = Trickster.mysql.getInstance();
-        msql.TryUserLogin(email,password);
-    }
+        msql.TryUserLogin(email, password);
 
-    if (msql.userType(email).equals("Employee")||msql.userType(email).equals("Customer")){
-        try {
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Menu.fxml")));
-            stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
+        //scene swicht to Menu
+        if (msql.userType(email).equals("Employee") || msql.userType(email).equals("Customer")) {
+            try {
+                Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Menu.fxml")));
+                Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
