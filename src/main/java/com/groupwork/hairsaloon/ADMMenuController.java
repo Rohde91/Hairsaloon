0de4a999b9ca251a3c557c8e9cc4a5fddeda2b70
alla2297 <alla2297@edu.easj.dk>
2022-04-21 13:35:25 +0200

    ADMMenuController switch scene to MyBooking

--- src/main/java/com/groupwork/hairsaloon/ADMMenuController.java
+++ src/main/java/com/groupwork/hairsaloon/ADMMenuController.java
@@ -108,7 +108,17 @@ public class ADMMenuController implements Initializable {
 
         @FXML
         void myBookingScene(ActionEvent event) {
-
+            try {
+                HairsaloonController hc = new HairsaloonController();
+                hc.user = null;
+                Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MyBookingScene.fxml")));
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         @FXML
