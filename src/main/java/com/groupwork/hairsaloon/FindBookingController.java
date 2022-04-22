9a74920c5a8099f6df6fabf1cb0bee34eebbad76
alla2297 <alla2297@edu.easj.dk>
2022-04-22 11:54:29 +0200

    choiceBox can now find see Employee's in FindBookingController and can swigth scene to findEmployee

--- src/main/java/com/groupwork/hairsaloon/FindBookingController.java
+++ src/main/java/com/groupwork/hairsaloon/FindBookingController.java
@@ -1,18 +1,28 @@
 package com.groupwork.hairsaloon;
+import Trickster.mysql;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
 
-public class FindBookingController {
+public class FindBookingController implements Initializable {
+    mysql msql = mysql.getInstance();
+
     @FXML
     private Button createBooking;
 
@@ -27,6 +37,20 @@ public class FindBookingController {
 
     @FXML
     private Button logoutButton;
+
+    @FXML
+    private TextField costumerPhoneNumber;
+
+    @FXML
+    private TableView<?> costumerBookingList;
+
+    @FXML
+    private ChoiceBox<?> ChoiceBoxFrisør;
+
+    @FXML
+    void costumerBookingList(ActionEvent event) {
+
+    }
 
     @FXML
     void createBookingScene(ActionEvent event) {
@@ -132,4 +156,15 @@ public class FindBookingController {
         }
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        // Set dropdowns
+        ChoiceBox choiceBoxEmployees = ChoiceBoxFrisør;
+        ArrayList employees = msql.loadEmployeeList();
+        choiceBoxEmployees.getItems().addAll(employees);
+        System.out.println(choiceBoxEmployees.toString());
+
+
+    }
 }
