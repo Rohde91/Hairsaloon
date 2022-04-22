11d1a263ea51ea2596635068719c05aff2cc5a34
alla2297 <alla2297@edu.easj.dk>
2022-04-22 13:30:05 +0200

    choice box work and get your choice

--- src/main/java/com/groupwork/hairsaloon/FindBookingController.java
+++ src/main/java/com/groupwork/hairsaloon/FindBookingController.java
@@ -1,5 +1,7 @@
 package com.groupwork.hairsaloon;
 import Trickster.mysql;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +12,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -22,6 +23,9 @@ import java.util.ResourceBundle;
 
 public class FindBookingController implements Initializable {
     mysql msql = mysql.getInstance();
+
+    String selectedPhoneNumber;
+    String selectedEmployee;
 
     @FXML
     private Button createBooking;
@@ -160,6 +164,7 @@ public class FindBookingController implements Initializable {
         }
     }
 
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
@@ -172,7 +177,47 @@ public class FindBookingController implements Initializable {
         ArrayList customer = msql.loadCustomerList();
         choiceBoxCustomer.getItems().addAll(customer);
 
+        //System.out.println(ChoiceBoxFrisør.getValue());
+        choiceBoxEmployees.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number number2) {
+                if (choiceBoxEmployees.getItems().get((Integer) number2) != null) {
+                    //treatmentName = choiceBoxEmployees.getItems().get((Integer) number2).toString();
+                    //System.out.println(choiceBoxEmployees.getItems().get((Integer) number2));
+                    selectedEmployee = choiceBoxEmployees.getItems().get((Integer) number2).toString();
 
+                }
+            }
+        });
+        choiceBoxCustomer.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number number2) {
+                if (choiceBoxCustomer.getItems().get((Integer) number2) != null) {
+                    //treatmentName = choiceBoxEmployees.getItems().get((Integer) number2).toString();
+                    //System.out.println(choiceBoxCustomer.getItems().get((Integer) number2));
+                    selectedPhoneNumber = choiceBoxCustomer.getItems().get((Integer) number2).toString();
+
+                }
+            }
+        });
 
     }
+
+
+/*    @FXML
+    void test(MouseEvent event) {
+        System.out.println("1 "+ChoiceBoxFrisør.getInputMethodRequests());
+        System.out.println("2 "+ChoiceBoxFrisør.getUserData());
+        System.out.println("3 "+ChoiceBoxFrisør.getValue());
+        System.out.println("4 "+ChoiceBoxFrisør.getViewOrder());
+        System.out.println("5 "+ChoiceBoxFrisør.getOnMouseClicked());
+        System.out.println("5 "+ChoiceBoxFrisør.getOnKeyTyped());
+
+    }*/
+
+//    public void test(javafx.scene.input.InputMethodEvent inputMethodEvent) {
+//        System.out.println("1 "+ChoiceBoxFrisør.getInputMethodRequests());
+//        System.out.println("2 "+ChoiceBoxFrisør.getUserData());
+//        System.out.println("3 "+ChoiceBoxFrisør.getValue());
+//    }
 }
